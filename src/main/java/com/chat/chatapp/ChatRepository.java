6f10bcf7baef5a71c0d585fdd398b07befc62ba6
Nhahan <kisy324@naver.com>
2021-09-26 00:14:42 +0900

    ChatRepository 생성

---
+++ src/main/java/com/chat/chatapp/ChatRepository.java
@@ -0,0 +1,13 @@
+package com.chat.chatapp;
+
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.data.mongodb.repository.Tailable;
+import reactor.core.publisher.Flux;
+
+public interface ChatRepository extends ReactiveMongoRepository<Chat, String> {
+
+    @Tailable // 커서를 닫지 않고 계속 유지(무한스트림)
+    @Query("{sender:?0, receiver:?1}")
+    Flux<Chat> FindBySender(String sender, String receiver);
+}
